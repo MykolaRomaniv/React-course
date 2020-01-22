7d8cc76c2675d2dabc2dbe90fcd21dc28fbfef5a
Mykola Romaniv <mykolaromaniw@gmail.com>
2020-01-23 01:23:02 +0200

    done with http requests

--- http_requests/src/index.js
+++ http_requests/src/index.js
@@ -3,6 +3,30 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://jsonplaceholder.typicode.com';
+axios.defaults.headers.common['Authorization'] = 'AUTH TOKEN';
+
+axios.interceptors.request.use(config => {
+    console.log(config);
+    //Edit config
+    return config;
+}, error => {               //catchs erros from all sources
+    console.log(error); 
+    return Promise.reject(error);  // forward it to error sousce
+});
+
+axios.interceptors.response.use(config => {
+    console.log(config);
+    //Edit config
+    return config;
+}, error => {
+    console.log(error);
+    return Promise.reject(error);
+});
+
+
 
 ReactDOM.render( <App />, document.getElementById( 'root' ) );
 registerServiceWorker();
